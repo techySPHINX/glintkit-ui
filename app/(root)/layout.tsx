9ec95181bee0c3c1dad37bf64ee58d8a79250a44
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-05-14 23:54:04 +0530

    refactor : support

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/landing/header";
+import { Header } from "@/components/landing/Header";
 import { Footer } from "@/components/layout/footer";
 import type { Metadata } from "next";
 // import { usePathname } from "next/navigation";
