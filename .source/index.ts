9f38d8d1b0c5ddd2eb75594c9aa3e3ca5e862dbd
techySphinx <138506773+techySPHINX@users.noreply.github.com>
2025-08-03 19:27:00 +0530

    feat(husky): setup

--- .source/index.ts
+++ .source/index.ts
@@ -1,39 +1,312 @@
-// @ts-nocheck -- skip type checking
-import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=1751038151335"
-import * as docs_32 from "../content/docs/hooks/use-auto-resize-textarea.mdx?collection=docs&hash=1751038151335"
-import * as docs_31 from "../content/docs/components/vercel-v0-chat.mdx?collection=docs&hash=1751038151335"
-import * as docs_30 from "../content/docs/components/tweet-card.mdx?collection=docs&hash=1751038151335"
-import * as docs_29 from "../content/docs/components/toolbar.mdx?collection=docs&hash=1751038151335"
-import * as docs_28 from "../content/docs/components/text.mdx?collection=docs&hash=1751038151335"
-import * as docs_27 from "../content/docs/components/profile.mdx?collection=docs&hash=1751038151335"
-import * as docs_26 from "../content/docs/components/pricing.mdx?collection=docs&hash=1751038151335"
-import * as docs_25 from "../content/docs/components/particles-background.mdx?collection=docs&hash=1751038151335"
-import * as docs_24 from "../content/docs/components/particle-button.mdx?collection=docs&hash=1751038151335"
-import * as docs_23 from "../content/docs/components/matrix-text.mdx?collection=docs&hash=1751038151335"
-import * as docs_22 from "../content/docs/components/list.mdx?collection=docs&hash=1751038151335"
-import * as docs_21 from "../content/docs/components/input.mdx?collection=docs&hash=1751038151335"
-import * as docs_20 from "../content/docs/components/hero-geometric.mdx?collection=docs&hash=1751038151335"
-import * as docs_19 from "../content/docs/components/hand-written-title.mdx?collection=docs&hash=1751038151335"
-import * as docs_18 from "../content/docs/components/faq.mdx?collection=docs&hash=1751038151335"
-import * as docs_17 from "../content/docs/components/currency-transfer.mdx?collection=docs&hash=1751038151335"
-import * as docs_16 from "../content/docs/components/checkout-interaction.mdx?collection=docs&hash=1751038151335"
-import * as docs_15 from "../content/docs/components/card.mdx?collection=docs&hash=1751038151335"
-import * as docs_14 from "../content/docs/components/button.mdx?collection=docs&hash=1751038151335"
-import * as docs_13 from "../content/docs/components/bento-grid.mdx?collection=docs&hash=1751038151335"
-import * as docs_12 from "../content/docs/components/beams-background.mdx?collection=docs&hash=1751038151335"
-import * as docs_11 from "../content/docs/components/background-paths.mdx?collection=docs&hash=1751038151335"
-import * as docs_10 from "../content/docs/components/background-circles.mdx?collection=docs&hash=1751038151335"
-import * as docs_9 from "../content/docs/components/avatar-picker.mdx?collection=docs&hash=1751038151335"
-import * as docs_8 from "../content/docs/components/alert.mdx?collection=docs&hash=1751038151335"
-import * as docs_7 from "../content/docs/components/ai-input.mdx?collection=docs&hash=1751038151335"
-import * as docs_6 from "../content/docs/components/action-search-bar.mdx?collection=docs&hash=1751038151335"
-import * as docs_5 from "../content/docs/blocks/minimal-shop.mdx?collection=docs&hash=1751038151335"
-import * as docs_4 from "../content/docs/blocks/dashboard.mdx?collection=docs&hash=1751038151335"
-import * as docs_3 from "../content/docs/blocks/auth-basic.mdx?collection=docs&hash=1751038151335"
-import * as docs_2 from "../content/docs/blocks/ai-chat.mdx?collection=docs&hash=1751038151335"
-import * as docs_1 from "../content/docs/blocks/ai-card-generation.mdx?collection=docs&hash=1751038151335"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1751038151335"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"blocks\\ai-card-generation.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/ai-card-generation.mdx"}, data: docs_1 }, { info: {"path":"blocks\\ai-chat.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/ai-chat.mdx"}, data: docs_2 }, { info: {"path":"blocks\\auth-basic.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/auth-basic.mdx"}, data: docs_3 }, { info: {"path":"blocks\\dashboard.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/dashboard.mdx"}, data: docs_4 }, { info: {"path":"blocks\\minimal-shop.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/minimal-shop.mdx"}, data: docs_5 }, { info: {"path":"components\\action-search-bar.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/action-search-bar.mdx"}, data: docs_6 }, { info: {"path":"components\\ai-input.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/ai-input.mdx"}, data: docs_7 }, { info: {"path":"components\\alert.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/alert.mdx"}, data: docs_8 }, { info: {"path":"components\\avatar-picker.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/avatar-picker.mdx"}, data: docs_9 }, { info: {"path":"components\\background-circles.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/background-circles.mdx"}, data: docs_10 }, { info: {"path":"components\\background-paths.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/background-paths.mdx"}, data: docs_11 }, { info: {"path":"components\\beams-background.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/beams-background.mdx"}, data: docs_12 }, { info: {"path":"components\\bento-grid.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/bento-grid.mdx"}, data: docs_13 }, { info: {"path":"components\\button.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/button.mdx"}, data: docs_14 }, { info: {"path":"components\\card.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/card.mdx"}, data: docs_15 }, { info: {"path":"components\\checkout-interaction.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/checkout-interaction.mdx"}, data: docs_16 }, { info: {"path":"components\\currency-transfer.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/currency-transfer.mdx"}, data: docs_17 }, { info: {"path":"components\\faq.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/faq.mdx"}, data: docs_18 }, { info: {"path":"components\\hand-written-title.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/hand-written-title.mdx"}, data: docs_19 }, { info: {"path":"components\\hero-geometric.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/hero-geometric.mdx"}, data: docs_20 }, { info: {"path":"components\\input.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/input.mdx"}, data: docs_21 }, { info: {"path":"components\\list.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/list.mdx"}, data: docs_22 }, { info: {"path":"components\\matrix-text.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/matrix-text.mdx"}, data: docs_23 }, { info: {"path":"components\\particle-button.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/particle-button.mdx"}, data: docs_24 }, { info: {"path":"components\\particles-background.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/particles-background.mdx"}, data: docs_25 }, { info: {"path":"components\\pricing.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/pricing.mdx"}, data: docs_26 }, { info: {"path":"components\\profile.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/profile.mdx"}, data: docs_27 }, { info: {"path":"components\\text.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/text.mdx"}, data: docs_28 }, { info: {"path":"components\\toolbar.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/toolbar.mdx"}, data: docs_29 }, { info: {"path":"components\\tweet-card.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/tweet-card.mdx"}, data: docs_30 }, { info: {"path":"components\\vercel-v0-chat.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/vercel-v0-chat.mdx"}, data: docs_31 }, { info: {"path":"hooks\\use-auto-resize-textarea.mdx","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/hooks/use-auto-resize-textarea.mdx"}, data: docs_32 }]);
-export const meta = _runtime.meta<typeof _source.meta>([{ info: {"path":"meta.json","absolutePath":"C:/Users/KIIT/Desktop/glintkit-ui/content/docs/meta.json"}, data: meta_0 }]);
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import * as meta_0 from "../content/docs/meta.json?collection=meta&hash=1751038151335";
+import * as docs_32 from "../content/docs/blocks/minimal-shop.mdx?collection=docs&hash=1751038151335";
+import * as docs_31 from "../content/docs/blocks/dashboard.mdx?collection=docs&hash=1751038151335";
+import * as docs_30 from "../content/docs/blocks/auth-basic.mdx?collection=docs&hash=1751038151335";
+import * as docs_29 from "../content/docs/blocks/ai-chat.mdx?collection=docs&hash=1751038151335";
+import * as docs_28 from "../content/docs/blocks/ai-card-generation.mdx?collection=docs&hash=1751038151335";
+import * as docs_27 from "../content/docs/components/vercel-v0-chat.mdx?collection=docs&hash=1751038151335";
+import * as docs_26 from "../content/docs/components/tweet-card.mdx?collection=docs&hash=1751038151335";
+import * as docs_25 from "../content/docs/components/toolbar.mdx?collection=docs&hash=1751038151335";
+import * as docs_24 from "../content/docs/components/text.mdx?collection=docs&hash=1751038151335";
+import * as docs_23 from "../content/docs/components/profile.mdx?collection=docs&hash=1751038151335";
+import * as docs_22 from "../content/docs/components/pricing.mdx?collection=docs&hash=1751038151335";
+import * as docs_21 from "../content/docs/components/particles-background.mdx?collection=docs&hash=1751038151335";
+import * as docs_20 from "../content/docs/components/particle-button.mdx?collection=docs&hash=1751038151335";
+import * as docs_19 from "../content/docs/components/matrix-text.mdx?collection=docs&hash=1751038151335";
+import * as docs_18 from "../content/docs/components/list.mdx?collection=docs&hash=1751038151335";
+import * as docs_17 from "../content/docs/components/input.mdx?collection=docs&hash=1751038151335";
+import * as docs_16 from "../content/docs/components/hero-geometric.mdx?collection=docs&hash=1751038151335";
+import * as docs_15 from "../content/docs/components/hand-written-title.mdx?collection=docs&hash=1751038151335";
+import * as docs_14 from "../content/docs/components/faq.mdx?collection=docs&hash=1751038151335";
+import * as docs_13 from "../content/docs/components/currency-transfer.mdx?collection=docs&hash=1751038151335";
+import * as docs_12 from "../content/docs/components/checkout-interaction.mdx?collection=docs&hash=1751038151335";
+import * as docs_11 from "../content/docs/components/card.mdx?collection=docs&hash=1751038151335";
+import * as docs_10 from "../content/docs/components/button.mdx?collection=docs&hash=1751038151335";
+import * as docs_9 from "../content/docs/components/bento-grid.mdx?collection=docs&hash=1751038151335";
+import * as docs_8 from "../content/docs/components/beams-background.mdx?collection=docs&hash=1751038151335";
+import * as docs_7 from "../content/docs/components/background-paths.mdx?collection=docs&hash=1751038151335";
+import * as docs_6 from "../content/docs/components/background-circles.mdx?collection=docs&hash=1751038151335";
+import * as docs_5 from "../content/docs/components/avatar-picker.mdx?collection=docs&hash=1751038151335";
+import * as docs_4 from "../content/docs/components/alert.mdx?collection=docs&hash=1751038151335";
+import * as docs_3 from "../content/docs/components/ai-input.mdx?collection=docs&hash=1751038151335";
+import * as docs_2 from "../content/docs/components/action-search-bar.mdx?collection=docs&hash=1751038151335";
+import * as docs_1 from "../content/docs/hooks/use-auto-resize-textarea.mdx?collection=docs&hash=1751038151335";
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1751038151335";
+import { _runtime } from "fumadocs-mdx";
+import * as _source from "../source.config";
+export const docs = _runtime.doc<typeof _source.docs>([
+  {
+    info: {
+      path: "index.mdx",
+      absolutePath: "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/index.mdx",
+    },
+    data: docs_0,
+  },
+  {
+    info: {
+      path: "hooks\\use-auto-resize-textarea.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/hooks/use-auto-resize-textarea.mdx",
+    },
+    data: docs_1,
+  },
+  {
+    info: {
+      path: "components\\action-search-bar.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/action-search-bar.mdx",
+    },
+    data: docs_2,
+  },
+  {
+    info: {
+      path: "components\\ai-input.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/ai-input.mdx",
+    },
+    data: docs_3,
+  },
+  {
+    info: {
+      path: "components\\alert.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/alert.mdx",
+    },
+    data: docs_4,
+  },
+  {
+    info: {
+      path: "components\\avatar-picker.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/avatar-picker.mdx",
+    },
+    data: docs_5,
+  },
+  {
+    info: {
+      path: "components\\background-circles.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/background-circles.mdx",
+    },
+    data: docs_6,
+  },
+  {
+    info: {
+      path: "components\\background-paths.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/background-paths.mdx",
+    },
+    data: docs_7,
+  },
+  {
+    info: {
+      path: "components\\beams-background.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/beams-background.mdx",
+    },
+    data: docs_8,
+  },
+  {
+    info: {
+      path: "components\\bento-grid.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/bento-grid.mdx",
+    },
+    data: docs_9,
+  },
+  {
+    info: {
+      path: "components\\button.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/button.mdx",
+    },
+    data: docs_10,
+  },
+  {
+    info: {
+      path: "components\\card.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/card.mdx",
+    },
+    data: docs_11,
+  },
+  {
+    info: {
+      path: "components\\checkout-interaction.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/checkout-interaction.mdx",
+    },
+    data: docs_12,
+  },
+  {
+    info: {
+      path: "components\\currency-transfer.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/currency-transfer.mdx",
+    },
+    data: docs_13,
+  },
+  {
+    info: {
+      path: "components\\faq.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/faq.mdx",
+    },
+    data: docs_14,
+  },
+  {
+    info: {
+      path: "components\\hand-written-title.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/hand-written-title.mdx",
+    },
+    data: docs_15,
+  },
+  {
+    info: {
+      path: "components\\hero-geometric.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/hero-geometric.mdx",
+    },
+    data: docs_16,
+  },
+  {
+    info: {
+      path: "components\\input.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/input.mdx",
+    },
+    data: docs_17,
+  },
+  {
+    info: {
+      path: "components\\list.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/list.mdx",
+    },
+    data: docs_18,
+  },
+  {
+    info: {
+      path: "components\\matrix-text.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/matrix-text.mdx",
+    },
+    data: docs_19,
+  },
+  {
+    info: {
+      path: "components\\particle-button.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/particle-button.mdx",
+    },
+    data: docs_20,
+  },
+  {
+    info: {
+      path: "components\\particles-background.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/particles-background.mdx",
+    },
+    data: docs_21,
+  },
+  {
+    info: {
+      path: "components\\pricing.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/pricing.mdx",
+    },
+    data: docs_22,
+  },
+  {
+    info: {
+      path: "components\\profile.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/profile.mdx",
+    },
+    data: docs_23,
+  },
+  {
+    info: {
+      path: "components\\text.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/text.mdx",
+    },
+    data: docs_24,
+  },
+  {
+    info: {
+      path: "components\\toolbar.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/toolbar.mdx",
+    },
+    data: docs_25,
+  },
+  {
+    info: {
+      path: "components\\tweet-card.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/tweet-card.mdx",
+    },
+    data: docs_26,
+  },
+  {
+    info: {
+      path: "components\\vercel-v0-chat.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/components/vercel-v0-chat.mdx",
+    },
+    data: docs_27,
+  },
+  {
+    info: {
+      path: "blocks\\ai-card-generation.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/ai-card-generation.mdx",
+    },
+    data: docs_28,
+  },
+  {
+    info: {
+      path: "blocks\\ai-chat.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/ai-chat.mdx",
+    },
+    data: docs_29,
+  },
+  {
+    info: {
+      path: "blocks\\auth-basic.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/auth-basic.mdx",
+    },
+    data: docs_30,
+  },
+  {
+    info: {
+      path: "blocks\\dashboard.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/dashboard.mdx",
+    },
+    data: docs_31,
+  },
+  {
+    info: {
+      path: "blocks\\minimal-shop.mdx",
+      absolutePath:
+        "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/blocks/minimal-shop.mdx",
+    },
+    data: docs_32,
+  },
+]);
+export const meta = _runtime.meta<typeof _source.meta>([
+  {
+    info: {
+      path: "meta.json",
+      absolutePath: "C:/Users/KIIT/Desktop/glintkit-ui/content/docs/meta.json",
+    },
+    data: meta_0,
+  },
+]);
